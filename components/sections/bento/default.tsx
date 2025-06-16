fd50b3afd58624dab3500618ccb74118fc6b30dd
Zhifan Pan <wojiaopanzhifan@gmail.com>
2025-06-16 11:18:18 +0800

    fix styles

--- components/sections/bento/default.tsx
+++ components/sections/bento/default.tsx
@@ -28,7 +28,7 @@ const features = [
   {
     icon: <Wrench className="inline-block h-12 w-12 m-2 origin-left stroke-1 text-neutral-950 dark:text-neutral-50" />,
     title: "网站搭建",
-    description: "专业的网站开发和设计服务，为您打造现代化、响应式的网站，确保良好的用户体验和功能性，助力您的在线业务发展。",
+    description: "一站式网站建设服务，量身定制响应式网站，兼容PC/移动多端访问。采用前沿开发框架与SEO友好架构，确保安全稳定运维",
     href: "/",
     cta: "了解更多",
     background: <div style={{ width: '100%', height: '100%', position: 'relative' }}>
@@ -43,7 +43,7 @@ const features = [
   {
     icon: <SearchCode className="inline-block h-12 w-12 m-2 origin-left stroke-1 text-neutral-950 dark:text-neutral-50" />,
     title: "数据分析",
-    description: "通过深入的市场调研和数据分析，帮助企业了解市场趋势、竞争格局和目标客户需求，为战略决策提供科学依据。",
+    description: "深度挖掘数据价值，驱动科学决策。用户行为追踪及多维度数据建模，精准分析流量转化路径、用户画像与业务增长瓶颈。",
     href: "/",
     cta: "了解更多",
     background: <Squares
@@ -58,7 +58,7 @@ const features = [
   {
     icon: <Cpu className="inline-block h-12 w-12 m-2 origin-left stroke-1 text-neutral-950 dark:text-neutral-50" />,
     title: "SEO 优化服务",
-    description: "通过专业的搜索引擎优化技术和策略，提升网站在搜索引擎中的排名和可见度，帮助企业获得持续稳定的自然流量。",
+    description: "让优质内容精准触达目标用户。基于搜索引擎算法规则，定制关键词矩阵布局、站内外优化及高质量外链建设方案。",
     href: "/",
     cta: "了解更多",
     background: <Aurora
